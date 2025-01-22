ec8f2b51d996fb5f8592fb8c46311e92a0a2f0a7
martin-ezequiel-barrera <barrea871009@gmail.com>
2025-01-22 07:12:10 +0100

    remove the contact info

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -21,17 +21,17 @@ function HeroSection() {
       />
 
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
+        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10 mt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
             This is {' '}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Professional `}
+            {`, I'm a Professional `}
             <span className=" text-[#16f2b3]">{personalData.designation}</span>
             .
           </h1>
 
-          <div className="my-12 flex items-center gap-5">
+          {/* <div className="my-12 flex items-center gap-5">
             <Link
               href={personalData.github}
               target='_blank'
@@ -67,9 +67,9 @@ function HeroSection() {
             >
               <FaTwitterSquare size={30} />
             </Link>
-          </div>
+          </div> */}
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3 mt-10">
             <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
               <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
                 <span>Contact me</span>
